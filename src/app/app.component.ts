e9f0c68b76b3e5b0bd9362e2617beca6a9c9f575
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-09-14 14:36:17 -0300

    config and methods init for geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { Observable } from 'rxjs';
 import { bindCallback } from 'rxjs';
 
 import { map } from 'rxjs/operators';
 
+const { Geolocation, Modals } = Plugins;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'capApp';
+  coords: Coordinates;
+
+  ngOnInit(){
+    // this.watchPosition().subscribe( coords =>
+    //   this.coords = coords
+    // );
+  }
+
+  // watchPosition(): Observable<any> {
+  //   const watch = bindCallback(Geolocation.watchPosition)({});
+
+  //   return watch.pipe(map(pos => pos.coords));
+  // }
+
+  showAlert() {
+    const lat = this.coords.latitude;
+    const lng = this.coords.longitude;
+    Modals.alert({
+      title: 'Your Position',
+      message: `Lat: ${lat}, Lng: ${lng}`,
+    });
+  }
+
 }
